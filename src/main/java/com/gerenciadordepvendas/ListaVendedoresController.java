68ca5c4ded44493668e87ea4bf9733f5da873de1
LeniseDeQueirozSilvaLozanoo <lenise.queiroz.silva@gmail.com>
2023-04-30 15:41:36 -0300

    Inclusão das colunas Data de nascimento e Salário base

--- src/main/java/com/gerenciadordepvendas/ListaVendedoresController.java
+++ src/main/java/com/gerenciadordepvendas/ListaVendedoresController.java
@@ -24,6 +24,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -40,6 +41,13 @@ public class ListaVendedoresController implements Initializable, DataChangeListe
     private TableColumn<Seller, Integer> tableColumnId;
     @FXML
     private TableColumn<Seller, String> tableColumnName;
+    @FXML
+    private TableColumn<Seller, String> tableColumnEmail;
+
+    @FXML
+    private TableColumn<Seller, Date> tableColumnBirthDate;
+    @FXML
+    private TableColumn<Seller, Double> tableColumnBaseSalary;
 
     @FXML
     private TableColumn<Seller, Seller> tableColumnEDIT;
@@ -71,6 +79,11 @@ public class ListaVendedoresController implements Initializable, DataChangeListe
     private void initializeNodes(){
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+        Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+        tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+        Utils.formatTableColumnDouble(tableColumnBaseSalary,2);
         Stage stage = (Stage) Application.getScene().getWindow();
         sellerTableView.prefHeightProperty().bind(stage.heightProperty());
     }
